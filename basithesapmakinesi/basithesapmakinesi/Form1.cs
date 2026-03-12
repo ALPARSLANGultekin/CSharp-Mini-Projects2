8495d5564d577e68af5067a9d2dce1371913942a
Gültekin ALPARSLAN <116844428+ALPARSLANGultekin@users.noreply.github.com>
2026-03-12 15:04:01 +0300

    Add files via upload

---
+++ basithesapmakinesi/basithesapmakinesi/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace basithesapmakinesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        double sayi1, sayi2, sonuc;
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            sayi1 = Convert.ToDouble(textBox1.Text);
+            sayi2 = Convert.ToDouble(textBox2.Text);
+            sonuc = sayi1 * sayi2;
+            textBox3.Text = sonuc.ToString("0.##");
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            sayi1 = Convert.ToDouble(textBox1.Text);
+            sayi2 = Convert.ToDouble(textBox2.Text);
+            sonuc = sayi1 / sayi2;
+            textBox3.Text = sonuc.ToString("0.##");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = " ";
+            textBox2.Text = " ";
+            textBox3.Text = " ";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            sayi1 = Convert.ToDouble(textBox1.Text);
+            sayi2 = Convert.ToDouble(textBox2.Text);
+            sonuc = sayi1 - sayi2;
+            textBox3.Text = sonuc.ToString("0.##");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sayi1 = Convert.ToDouble(textBox1.Text);
+            sayi2 = Convert.ToDouble(textBox2.Text);
+            sonuc = sayi1 + sayi2;
+            textBox3.Text = sonuc.ToString("0.##");
+        }
+    }
+}
